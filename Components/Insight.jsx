c841961fbdc35856d11543a51cfa285b3368c860
Rudalph <gonsalvesrudalph@gmail.com>
2025-01-11 15:43:21 +0530

    updates

--- Components/Insight.jsx
+++ Components/Insight.jsx
@@ -96,7 +96,7 @@ function Insight() {
   useEffect(() => {
     const fetchInsights = async () => {
       try {
-        const response = await fetch('http://localhost:5001/generate-insights');
+        const response = await fetch('http://127.0.0.1:5001/generate-insights');
         const data = await response.json();
         // Assuming your backend returns insights as an array of objects
         const formattedInsights = data.json_output; // Convert points object to an array
